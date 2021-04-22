c3e74449aab30469b1baa2b68691d924e1d4c64d
j0k3r <j0k3r@yandex.ru>
2021-04-22 09:41:25 +0300

    unit tests added

---
+++ AzureTestAppTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzureTestAppTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const string Expected = "Hello World!";
+        [TestMethod]
+        public void TestMethod1()
+        {
+            using (var sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                AzureTestApp.Program.Main();
+
+                var result = sw.ToString().Trim();
+                Assert.AreEqual(Expected, result);
+            }
+        }
+    }
+}
